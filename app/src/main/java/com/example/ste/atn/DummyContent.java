a0aeb586162a664af25b750adaae6dfd77828c42
Romeo Jovel <jovelromeo@gmail.com>
2016-11-11 18:27:44 -0300

    primer comit
    app atn

---
+++ app/src/main/java/com/example/ste/atn/DummyContent.java
@@ -0,0 +1,8 @@
+package com.example.ste.atn;
+
+/**
+ * Created by rojo on 10/11/2016.
+ */
+
+public class DummyContent {
+}
---
+++ app/src/main/java/com/example/ste/atn/model/Evento.java
@@ -0,0 +1,11 @@
+package com.example.ste.atn.model;
+
+/**
+ * Created by rojo on 10/11/2016.
+ */
+
+public class Evento {
+    Integer id;
+    String descripcion;
+
+}
